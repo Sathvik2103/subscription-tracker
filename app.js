9baa583f9da0d74ed360eb5d2e1ad5e87b53eb3e
Sathvik <bhatsathvik21@gmail.com>
2025-02-11 20:31:28 +0530

    My first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscriptions.routes.js';
+import connectDB from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware );
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, async() => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+
+    await connectDB();
+});
+
+
+export default app;
